1aee046bb017e8742a8607a5f63adc0351519ff6
henneth <henneth.cheng@gmail.com>
2020-05-17 22:31:21 +0800

    update

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,19 +1,19 @@
 <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav ml-auto" style="font-weight: 700;letter-spacing: 0.6px;">
-        <li class="nav-item {{ Request::path() == '/a' ? 'active' : '' }}">
-            <a class="nav-link" href="{{url('/')}}">TU重組方案</a>
+        <li class="nav-item {{ Request::path() == '/tu-debt-restructuring' ? 'active' : '' }}">
+            <a class="nav-link" href="{{url('/tu-debt-restructuring')}}">TU重組方案</a>
         </li>
-        <li class="nav-item {{ Request::path() == '/b' ? 'active' : '' }}">
-            <a class="nav-link" href="{{url('/')}}">債務舒緩</a>
+        <li class="nav-item {{ Request::path() == '/debt-relief-loan' ? 'active' : '' }}">
+            <a class="nav-link" href="{{url('/debt-relief-loan')}}">債務舒緩</a>
         </li>
-        <li class="nav-item {{ Request::path() == '/c' ? 'active' : '' }}">
-            <a class="nav-link" href="{{url('/')}}">物業套現</a>
+        <li class="nav-item {{ Request::path() == '/mortgage-loan' ? 'active' : '' }}">
+            <a class="nav-link" href="{{url('/mortgage-loan')}}">物業套現</a>
         </li>
-        <li class="nav-item {{ Request::path() == '/d' ? 'active' : '' }}">
-            <a class="nav-link" href="{{url('/')}}">加按套現</a>
+        <li class="nav-item {{ Request::path() == '/property-refinancing' ? 'active' : '' }}">
+            <a class="nav-link" href="{{url('/property-refinancing')}}">加按套現</a>
         </li>
-        <li class="nav-item {{ Request::path() == '/e' ? 'active' : '' }}">
-            <a class="nav-link" href="{{url('/')}}">聯絡我們</a>
+        <li class="nav-item {{ Request::path() == '/contact-us' ? 'active' : '' }}">
+            <a class="nav-link" href="{{url('/contact-us')}}">聯絡我們</a>
         </li>
     </ul>
 </div>
